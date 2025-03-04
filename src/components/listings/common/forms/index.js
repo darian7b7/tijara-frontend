3ee1d40de90fee4e978fe00ab7f5b6178ed24eea
darian7b7 <daryannabo16@gmail.com>
2025-03-04 17:00:23 +0000

    Deleted all files

--- src/components/listings/common/forms/index.js
+++
@@ -1,7 +0,0 @@
-// Export all form components from a single file
-export { default as FormInput } from './FormInput';
-export { default as FormNumberInput } from './FormNumberInput';
-export { default as FormSelect } from './FormSelect';
-export { default as CheckboxGroup } from './CheckboxGroup';
-export { default as SelectDropdown } from './SelectDropdown';
-export { default as InputField } from './InputField';
